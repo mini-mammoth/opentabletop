88f79d97ec538c8b0618668e3c638cc5ce216d87
Paul Reichelt <reicheltp@users.noreply.github.com>
2020-04-11 19:37:27 +0200

    Fix db proxy (#7)
    * Extract PouchDBContext
    
    * Use container to restrict AppBar content width
    
    * Fix: Use node http api to pipe requests to couchdb

--- packages/portal/components/layout/Header.js
+++ packages/portal/components/layout/Header.js
@@ -1,5 +1,6 @@
 import AppBar from '@material-ui/core/AppBar'
 import Button from '@material-ui/core/Button'
+import Container from '@material-ui/core/Container'
 import makeStyles from '@material-ui/core/styles/makeStyles'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
@@ -9,6 +10,9 @@ const useStyles = makeStyles(
   (theme) => ({
     root: {
       flexGrow: 1,
+    },
+    container: {
+      display: 'inline-flex',
     },
     title: {
       flexGrow: 1,
@@ -23,10 +27,12 @@ function Header() {
   return (
     <AppBar position="static">
       <Toolbar>
-        <Typography variant="h6" className={classes.title}>
-          Open Table Top
-        </Typography>
-        <Button color="inherit">Login</Button>
+        <Container className={classes.container}>
+          <Typography variant="h6" className={classes.title}>
+            Open Table Top
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Container>
       </Toolbar>
     </AppBar>
   )
---
+++ packages/portal/utils/PouchDBContext.js
@@ -0,0 +1,50 @@
+import PouchDB from 'pouchdb'
+import React, { useContext, useEffect, useState } from 'react'
+
+const PouchDBContext = React.createContext(null)
+PouchDBContext.displayName = 'pouchdb Context'
+
+/**
+ * Provides a PouchDB instance for all children.
+ *
+ * Instance can be received can be accessed with `usePouchDB()`
+ * @param children {React.ReactNode}
+ * @param remoteUrl {string} - url of the remote database
+ */
+export default function PouchDBProvider({ children, remoteUrl }) {
+  const [db, setDB] = useState(undefined)
+
+  useEffect(() => {
+    if (!remoteUrl) {
+      return
+    }
+
+    if (remoteUrl[0] === '/') {
+      remoteUrl = `${window.location.origin}${remoteUrl}`
+    }
+
+    const db = new PouchDB(remoteUrl, {
+      live: true,
+    })
+
+    if (window) {
+      window.ACTIVE_DB = db
+    }
+
+    setDB(db)
+
+    db.info().then(console.log)
+  }, [remoteUrl])
+
+  return (
+    <PouchDBContext.Provider value={db}>{children}</PouchDBContext.Provider>
+  )
+}
+
+/**
+ * Returns the instance of the PouchDB.
+ * @return {PouchDB}
+ */
+export function usePouchDB() {
+  return useContext(PouchDBContext)
+}
