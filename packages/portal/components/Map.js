5ed7d9f7ca3e226e95fd977c5239485b1c2fe191
Sebastian Kasanzew <sebastian.kasanzew@regiocom.com>
2020-04-11 18:39:07 +0200

    Make Pixi stage responsive

--- packages/portal/components/Map.js
+++ packages/portal/components/Map.js
@@ -1,28 +1,52 @@
 import { Stage, Text } from '@inlet/react-pixi'
 import { makeStyles } from '@material-ui/core/styles'
-import React from 'react'
-
-let width = 500
+import React, { useEffect } from 'react'
+import { useMeasure } from 'react-use'
 
 const useStyles = makeStyles(() => ({
   root: {
-    width,
+    position: 'relative',
+    width: '100%',
+    height: '100%',
+    minHeight: 300,
+    minWidth: 200,
+    overflow: 'hidden',
+  },
+  stage: {
+    position: 'absolute',
+    overflow: 'hidden',
   },
 }))
 
+const stageOptions = {
+  backgroundColor: 0x1099bb,
+  resolution: window.devicePixelRatio,
+}
+
+let app
+
 function Map() {
-  const { root } = useStyles()
+  if (!window) {
+    return <div>{'This component only works in the browser'}</div>
+  }
+
+  const { root, stage } = useStyles()
+  const [ref, { width, height }] = useMeasure()
+
+  useEffect(() => {
+    app?.renderer.resize(width, height)
+  }, [width, height])
 
   return (
-    <Stage
-      options={{
-        backgroundColor: 0x1099bb,
-        width,
-      }}
-      className={root}
-    >
-      <Text x={30} y={90} text="Test text" />
-    </Stage>
+    <div ref={ref} className={root}>
+      <Stage
+        className={stage}
+        options={stageOptions}
+        onMount={(newApp) => (app = newApp)}
+      >
+        <Text x={30} y={90} text="Test text" />
+      </Stage>
+    </div>
   )
 }
 
--- packages/portal/pages/games/[game_id].js
+++ packages/portal/pages/games/[game_id].js
@@ -15,6 +15,7 @@ const useStyles = makeStyles(() => ({
   root: {
     display: 'flex',
     flexDirection: 'column',
+    height: '100vh',
   },
 }))
 
